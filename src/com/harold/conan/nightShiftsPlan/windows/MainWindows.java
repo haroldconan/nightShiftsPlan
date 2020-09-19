6509464f1e95813a1e552091a3851e03a906624e
28har <28har@DESKTOP-LPLSODQ.home>
2020-09-19 11:56:25 +0200

    test commit

--- src/com/harold/conan/nightShiftsPlan/windows/MainWindows.java
+++ src/com/harold/conan/nightShiftsPlan/windows/MainWindows.java
@@ -3,6 +3,8 @@ package com.harold.conan.nightShiftsPlan.windows;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
 
 public class MainWindows {
 
@@ -43,7 +45,9 @@ public class MainWindows {
 		shlNightShiftsPlanning = new Shell();
 		shlNightShiftsPlanning.setSize(1150, 845);
 		shlNightShiftsPlanning.setText("Night shifts planning");
+		
+		Composite composite = new Composite(shlNightShiftsPlanning, SWT.NONE);
+		composite.setBounds(130, 45, 505, 294);
 
 	}
-
 }
